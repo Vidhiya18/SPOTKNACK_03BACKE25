5654a2655429d45ee2cbc3bfc1fbc288f6e6d481
Vidhiya18 <cvidhiyalakshmi@gmail.com>
2025-09-13 11:35:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main{
+    /**
+     * the default age for the person.
+     * @param args
+     */
+    public static final int AGE=18;
+    public static void main(String[]args){
+        //this is the main method ,program starts here.
+        int age=18;//this is an integer variable to store age
+        String name="Vidhiya";
+        // this is a string variable to store name
+        String address="Tenkasi";
+        // this is a string variable to store address
+        String hobby="dancing";
+        // this is a string variable to store hobby
+        String favouritecolour="lavendar";
+        // this is a string variable to store favouritecolour
+        {
+            //printing the values
+            System.out.println(age);
+            System.out.println(name);
+            System.out.println(address);
+            System.out.println(hobby);
+            System.out.println(favouritecolour);
+            System.out.println("age:" + AGE);
+        }
+    }
+}
